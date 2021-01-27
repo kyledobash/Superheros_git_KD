48073e396787034503f81c50073ad579fce31ce8
kyledobash <kyledobash@yahoo.com>
2021-01-27 13:17:38 -0700

    added HeroController and Create View for the Heroes Model

---
+++ Superheroes_Actual_KD/Controllers/HeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Superheroes_Actual_KD.Models;
+
+namespace Superheroes_Actual_KD.Controllers
+{
+    public class HeroController : Controller
+    {
+        public HeroController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
