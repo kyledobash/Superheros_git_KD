0042ce0778925862056a6f4c71b149e274640e35
kyledobash <kyledobash@yahoo.com>
2021-01-27 12:51:23 -0700

    Added Migration and DbSet

--- Superheroes_Actual_KD/Data/ApplicationDbContext.cs
+++ Superheroes_Actual_KD/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superheroes_Actual_KD.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Superheroes_Actual_KD.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheroes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
