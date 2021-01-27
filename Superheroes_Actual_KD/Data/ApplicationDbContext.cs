3d6ceb6f6e8ae3b35b0df371131ded29ddb512e3
kyledobash <kyledobash@yahoo.com>
2021-01-27 12:30:42 -0700

    init

---
+++ Superheroes_Actual_KD/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Superheroes_Actual_KD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Superheroes_Actual_KD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Superheroes_Actual_KD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
